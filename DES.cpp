685bc43becc74896d486d69357aea2d6370afc8c
Ben <bsattelb@mymail.mines.edu>
2014-11-09 00:48:21 -0700

    Added support for full sequential code
    Untested

--- DES.cpp
+++ DES.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
+#include <cmath>
 //#include "mpi.h"
 using namespace std;
 
@@ -233,25 +234,80 @@ void generateLR(bool* L[], bool* R[], char* text[]) {
 		L[i + 5] = text[i] & 4;
 		L[i + 6] = text[i] & 2;
 		L[i + 7] = text[i] & 1;
-	}
-	for (int i = 4; i < 8; ++i) {
-		R[i + 0] = text[i] & 128;
-		R[i + 1] = text[i] & 64;
-		R[i + 2] = text[i] & 32;
-		R[i + 3] = text[i] & 16;
-		R[i + 4] = text[i] & 8;
-		R[i + 5] = text[i] & 4;
-		R[i + 6] = text[i] & 2;
-		R[i + 7] = text[i] & 1;
+		
+		R[i + 0] = text[i+4] & 128;
+		R[i + 1] = text[i+4] & 64;
+		R[i + 2] = text[i+4] & 32;
+		R[i + 3] = text[i+4] & 16;
+		R[i + 4] = text[i+4] & 8;
+		R[i + 5] = text[i+4] & 4;
+		R[i + 6] = text[i+4] & 2;
+		R[i + 7] = text[i+4] & 1;
 	}
 }
+
+void generateText(bool* L[], bool* R[], char* text[]) {
+	for (int i = 0; i < 8; ++i) {
+		// Use the values in L and R to generate characters
+		int cha = 0;
+		for (int j = 0; j < 8; ++j) {
+			// Big-endian, so 7-i
+			if (i < 4) {
+				cha += L[j] * pow(2, 7 - j);
+			}
+			else {
+				cha += R[j] * pow(2, 7 - j);
+			}
+		}
+		text[i] = (char) cha;
+	}
+}
+
+void readIn(char* [] text, ifstream* file) {
+	// Initialize every element to the null character
+	// So that we can know when the end is reached
+	for (int i = 0; i < 8; ++i) {
+		text[i] = '\0';
+	}
+	// Read in eight characters
+	file.read(text[0], 8);
+}
+
+writeOut(char* [] text, ofstream outFile) {
+	for (int i = 0; i < 8; ++i) {
+		// Don't output the null character
+		if (text[i] != '\0') {
+			outFile.put(text[i]);
+		}
+	}
+}
+
 int main(int argc, char* argv[]) {
 	bool* L = new bool[32];
 	bool* R = new bool[32];
 	bool* key = new bool[64];
-	ifstream file;
-	file.open("input.txt");
-	generateLR(L, R, text);
-	applyDES(L, R, key, true);
+	char* text = new char[8];
+	
+	ifstream* inFile;
+	inFile.open("input.txt");
+	inFile.seekg (0, inFile.end);
+	int length = inFile.tellg();
+	inFile.seekg(0, inFile.beg);
+	
+	ofstream* outFile;
+	outFile.open("out.txt");
+	
+	for (int i = 0; i < (length / 8) + 1; ++i) {
+		readIn(text, inFile);
+		generateLR(L, R, text);
+		
+		applyDES(L, R, key, true);
+		
+		generateOutText(L, R, text);
+		writeOut(text, outFile);
+	}
+	// Terminate the file with a null character
+	outFile.put('\0');
+	
 	return 0;
 }
