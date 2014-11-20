c0d0ee6c4a0f8f06a6177eb23625dd6a020577cc
Ben <bsattelb@mymail.mines.edu>
2014-11-20 14:22:15 -0700

    Increased correctness of brute force algorithm

--- bruteForce.cpp
+++ bruteForce.cpp
@@ -13,24 +13,43 @@ int main(int argc, char* argv[]) {
 	bool* R = new bool[32];
 	bool* keyBool = new bool[64];
 	char* text = new char[8];
-	
-	ifstream* inFile = new ifstream("input.txt");
+	char* signature = new char[8];
+	ifstream* inFile = new ifstream("input.txt", ios::binary);
 	inFile->seekg (0, inFile->end);
-	int length = inFile->tellg();	
+	int length = inFile->tellg();
 	
+	bool temp;
+ 
 	for(int key = 0; key < pow(2, 56); ++key) {
-		// Code to calculate useable keys
+		for (int i = 0; i < 64; ++i) {
+			if (i % 8 == 0) {}
+			else {
+				keyBool[i] = key & (int)pow(2, (i- i /8));
+			}
+		}
 		inFile->seekg(0, inFile->beg);
 		
 		for (int i = 0; i < (length / 8) + 1; ++i) {
 			readIn(text, inFile);
 			generateLR(L, R, text);
 			
-			applyDES(L, R, keyBool, true);
+			applyDES(L, R, keyBool, false);
 			generateText(L, R, text);
-			// Check if portion of signature detected
-			// If so, keep it to test against next time
+			
+			temp = true;
+			for (int i = 0; i < 8; ++i) {
+				temp = temp && text[i] == signature[i];
+			}
+			if (temp) {
+				break;
+			}
+			
+		}
+		if (temp) {
+			// Output key
+			break;
 		}
 	}
+	
 	return 0;
 }
