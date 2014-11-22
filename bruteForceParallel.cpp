ae7d37c79e62de9d511d86117a8fae729a3264ae
kkalmbac <kkalmbac@mines.edu>
2014-11-21 17:03:20 -0700

    Fixed some problems with Brute Force Parallel

--- bruteForceParallel.cpp
+++ bruteForceParallel.cpp
@@ -18,19 +18,23 @@ int main(int argc, char* argv[]) {
 	bool* keyBool = new bool[64];
 	char* text = new char[8];
 	char* signature = new char[8];
-	ifstream* inFile = new ifstream("input.txt", ios::binary);
-	inFile->seekg (0, inFile->end);
-	int length = inFile->tellg();
+	ifstream inFile;
+	inFile.open("output.txt", ios::binary);
+	inFile.seekg (0, inFile.end);
+	int length = inFile.tellg();
+	for(int i = 0; i < 8; i++)
+		signature[i] = 'a';
   struct key_val{
     int isKey;
     int keyNum;
   } inKey, outKey;
   inKey.isKey = 0;
-  inkey.keyNum = 0;
+  inKey.keyNum = 0;
 	
 	bool temp;
-  int num_keys = pow(2,56)/num_cores;
-
+  //int num_keys = pow(2,56)/num_cores;
+  int num_keys = 100/num_cores;
+  cout<<"we got here! "<< my_rank<< endl;
   if(my_rank != num_cores - 1) {
 	for(int key = my_rank*num_keys; key < my_rank*num_keys+num_keys ; ++key) {
 		for (int i = 0; i < 64; ++i) {
@@ -39,10 +43,10 @@ int main(int argc, char* argv[]) {
 				keyBool[i] = key & (int)pow(2, (i- i /8));
 			}
 		}
-		inFile->seekg(0, inFile->beg);
+		inFile.seekg(0, inFile.beg);
 		
 		for (int i = 0; i < (length / 8) + 1; ++i) {
-			readIn(text, inFile);
+			readIn(text, &inFile);
 			generateLR(L, R, text);
 			
 			applyDES(L, R, keyBool, false);
@@ -66,17 +70,17 @@ int main(int argc, char* argv[]) {
   }
 
   else {
-	for(int key = my_rank*num_keys; key < pow(2,56) ; ++key) {
+	for(int key = my_rank*num_keys; key < 100 ; ++key) {
 		for (int i = 0; i < 64; ++i) {
 			if (i % 8 == 7) {}
 			else {
 				keyBool[i] = key & (int)pow(2, (i- i /8));
 			}
 		}
-		inFile->seekg(0, inFile->beg);
+		inFile.seekg(0, inFile.beg);
 		
 		for (int i = 0; i < (length / 8) + 1; ++i) {
-			readIn(text, inFile);
+			readIn(text, &inFile);
 			generateLR(L, R, text);
 			
 			applyDES(L, R, keyBool, false);
@@ -99,12 +103,14 @@ int main(int argc, char* argv[]) {
 	}
   }
 
-  MPI::COMM_WORLD.Reduce( &inKey, &outKey, 1, MPI::INT_INT, MPI::MAXLOC, 0);
-  cout << "The key to decrypt this file is: " << outKey.keyNum << endl;
+  MPI::COMM_WORLD.Reduce( &inKey, &outKey, 1, MPI::TWOINT, MPI::MAXLOC, 0);
+  if(my_rank == 0)
+  	cout << "The key to decrypt this file is: " << outKey.keyNum << endl;
 
 
   double end_time = MPI::Wtime();
-	double time = end_time - begin_time;
+  double time = end_time - begin_time;
+  cout << "Time taken: " << time;
   MPI::Finalize();
 
 	return 0;
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 CC = g++
 mpi = mpiCC
 
-all: fullySequential bruteForce simplyParallel openMPMain
+all: fullySequential bruteForce simplyParallel openMPMain bruteParallel
 
 fullySequential: fullySequential.o desSequentialAlgorithm.o fileAndConversion.o
 	$(CC) -o fullySequential fullySequential.o desSequentialAlgorithm.o fileAndConversion.o
@@ -13,6 +13,9 @@ simplyParallel: simplyParallel.o fileAndConversion.o desSequentialAlgorithm.o
 
 openMPMain: openMPMain.o fileAndConversion.o desSequentialAlgorithm.o
 	$(CC) -fopenmp -o openMPMain openMPMain.o fileAndConversion.o desSequentialAlgorithm.o
+	
+bruteParallel: desSequentialAlgorithm.o fileAndConversion.o bruteForceParallel.o
+	$(mpi) -o bruteParallel desSequentialAlgorithm.o fileAndConversion.o bruteForceParallel.o
 
 fullySequential.o: fullySequential.cpp
 	$(CC) -c fullySequential.cpp
@@ -31,6 +34,9 @@ simplyParallel.o: simplyParallel.cpp
 
 openMPMain.o: openMPMain.cpp
 	$(CC) -fopenmp -c openMPMain.cpp
+
+bruteForceParallel.o: bruteForceParallel.cpp
+	$(mpi) -c bruteForceParallel.cpp	
 	
 clean:
-	rm -rf *.o fullySequential bruteForce simplyParallel openMPMain
+	rm -rf *.o fullySequential bruteForce simplyParallel openMPMain bruteParallel
